21b03046b3871f9079a22b5c632125f4ff069c50
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-15 06:35:31 +0200

    :sparkles: add navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import React, { useState } from "react";
+import { Container, Nav, Navbar, Button, Offcanvas } from "react-bootstrap";
+import logo from "../images/MKT.svg";
+import { FaBars } from "react-icons/fa";
 
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const navArray = [
+    { id: 1, url: "#features", title: "Features" },
+    { id: 2, url: "#projects", title: "Projects" },
+    { id: 3, url: "#contact", title: "Contact" },
+  ];
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container className="align-items-center">
+        <Link to="/" className="navbar-brand">
+          <img src={logo} alt="MKT" />
+        </Link>
+        <Nav className="ms-auto d-none d-lg-flex text-primary">
+          {navArray.map((nav) => {
+            const { id, url, title } = nav;
+            return (
+              <Nav.Link key={id} href={url}>
+                {title}
+              </Nav.Link>
+            );
+          })}
+        </Nav>
+        <Button
+          variant="transparent"
+          className="text-primary d-lg-none"
+          onClick={handleShow}
+        >
+          <FaBars size="1.5em" />
+        </Button>
+        <Offcanvas placement="end" show={show} onHide={handleClose}>
+          <Offcanvas.Header className="ms-auto" closeButton></Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="sidebar_nav flex-column">
+              {navArray.map((nav) => {
+                const { id, url, title } = nav;
+                return (
+                  <Nav.Link onClick={handleClose} key={id} href={url}>
+                    {title}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
