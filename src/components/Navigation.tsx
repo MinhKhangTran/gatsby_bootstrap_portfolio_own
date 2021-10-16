fe484882559ba7e4a9362ff02ed8e51a5411e483
MinhKhangTran <minhkhang.tran@gmx.de>
2021-10-16 08:43:23 +0200

    :sparkles: change offcanvas to navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,7 @@
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
-import React, { useState } from "react";
-import { Container, Nav, Navbar, Button, Offcanvas } from "react-bootstrap";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import logo from "../images/MKT.svg";
-import { FaBars } from "react-icons/fa";
 
 const Navigation = () => {
   const navArray = [
@@ -11,50 +9,26 @@ const Navigation = () => {
     { id: 2, url: "#projects", title: "Projects" },
     { id: 3, url: "#contact", title: "Contact" },
   ];
-  const [show, setShow] = useState(false);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
     <Navbar bg="white" expand="lg">
       <Container className="align-items-center">
         <Link to="/" className="navbar-brand">
           <img src={logo} alt="MKT" />
         </Link>
-        <Nav className="ms-auto d-none d-lg-flex ">
-          {navArray.map((nav) => {
-            const { id, url, title } = nav;
-            return (
-              <Nav.Link className="text-primary" key={id} href={url}>
-                {title}
-              </Nav.Link>
-            );
-          })}
-        </Nav>
-        <Button
-          variant="transparent"
-          className="text-primary d-lg-none"
-          onClick={handleShow}
-          aria-label="toggle Navigation"
-        >
-          <span className="visually-hidden">toggle Navigation</span>
-          <FaBars size="1.5em" />
-        </Button>
-        <Offcanvas placement="end" show={show} onHide={handleClose}>
-          <Offcanvas.Header className="ms-auto" closeButton></Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="sidebar_nav flex-column">
-              {navArray.map((nav) => {
-                const { id, url, title } = nav;
-                return (
-                  <Nav.Link key={id} href={url}>
-                    {title}
-                  </Nav.Link>
-                );
-              })}
-            </Nav>
-          </Offcanvas.Body>
-        </Offcanvas>
+        <Navbar.Toggle aria-controls="my-navbar-nav" />
+        <Navbar.Collapse id="my-navbar-nav">
+          <Nav className="ms-auto">
+            {navArray.map((nav) => {
+              const { id, url, title } = nav;
+              return (
+                <Nav.Link className="text-primary" key={id} href={url}>
+                  {title}
+                </Nav.Link>
+              );
+            })}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
