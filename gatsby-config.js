ef7eb1240cb48897f62fe7247386e245b67439d9
MinhKhangTran <minhkhang.tran@gmx.de>
2021-11-21 07:56:23 +0100

    :sparkles: add gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,7 @@ module.exports = {
     },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-gatsby-cloud",
     {
       resolve: "gatsby-source-filesystem",
       options: {
